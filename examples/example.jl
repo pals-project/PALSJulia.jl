8ed604bd5fb1c4bd0cef33dfc849e9d7620e9045
Alexheeeee <adh236@cornell.edu>
2026-06-18 22:06:40 -0700

    update examples/example.jl

--- examples/example.jl
+++ examples/example.jl
@@ -2,38 +2,63 @@ import Pkg
 Pkg.activate(joinpath(@__DIR__, ".."))
 import pals_julia as pj
 
-#reading a lattice from a yaml file
-yaml_file = abspath(joinpath(@__DIR__, "..", "lattice_files", "ex.pals.yaml"))
-node = pj.parse_file(yaml_file)
-#printing to terminal
-println(pj.to_yaml_string(node))
+function main()
+    lattice_dir = joinpath(@__DIR__, "..", "lattice_files")
+    ex_file      = joinpath(lattice_dir, "ex.pals.yaml")
+    expand_file  = joinpath(lattice_dir, "expand.pals.yaml")
 
-#type checking
-println((pj.is_sequence(node)))
+    println("============ Printing Developer Information ============")
 
-#accessing sequence
-seq = pj.getindex(node, 1)
-println("the first element is: \n", pj.to_yaml_string(seq))
+    text = "Use the function 'parse_file(filename)' to read a lattice file. For example,\n" *
+           "tree = pj.parse_file(\"../lattice_files/ex.pals.yaml\")\n" *
+           "reads the file 'ex.pals.yaml' into a tree named 'tree'.\n\n"
 
-#accessing map
-println("the value at key 'thingB' is: ", pj.to_yaml_string(getindex(seq, "thingB")))
+    # reading a lattice from a yaml file
+    print(text)
+    tree = pj.parse_file(ex_file)
 
-#creating a new node that's a map 
-map = pj.create_map()
-pj.setvalue!(map, 2, "first")
+    # printing to terminal
+    println("To print a tree to console, use the 'to_yaml_string(tree_name)' function.")
+    println(pj.to_yaml_string(tree), "\n")
 
-#creating a new node that's a sequence
-sequence = pj.create_sequence()
-pj.push!(sequence, "magnet1")
-pj.push!(sequence, "")
-scalar = pj.create_scalar()
-pj.set!(scalar, "magnet2")
-pj.set_at_index!(sequence, 1, scalar)
+    # type checking
+    println("The root node of 'ex.pals.yaml' is a sequence, so is_sequence(tree) = ",
+             pj.is_sequence(tree))
 
-#adding new nodes to lattice
-pj.push!(node, map)
-pj.push!(node, sequence)
+    # accessing sequence
+    println("Elements in a sequence may be accessed by their index.")
+    seq1 = tree[1]
+    println("The first element of 'tree' is: \n", pj.to_yaml_string(seq1))
 
-#writing modified lattice file to expand.pals.yaml
-file_dest = abspath(joinpath(@__DIR__, "..", "lattice_files", "expand.pals.yaml"))
-pj.write_yaml(node, file_dest)
+    # accessing map
+    println("Elements in a map may be accessed by their key.")
+    map1 = seq1[1]["kind"]
+    println("The element 'thingB' has:\n    ", pj.to_yaml_string(map1))
+
+    # add a new sequence element to the root containing new_map: {apples: 5}
+    println("Adding a new element '-apples: 5' to root.")
+    new_map_entry = pj.add_map!(tree)
+    map_node = pj.add_map!(new_map_entry, key="new_map")
+    pj.add_scalar!(map_node, "5", key="apples")
+
+    # add a new sequence element to the root containing magnets
+    println("Adding a new element")
+    println("    - magnet_list:")
+    println("        - magnet1")
+    println("        - magnet2")
+    println("to root.\n")
+    magnets_entry = pj.add_map!(tree)
+    sequence = pj.add_sequence!(magnets_entry, key="magnet_list")
+    pj.add_scalar!(sequence, "magnet1")
+    pj.add_scalar!(sequence, "magnet2", index=1)
+
+    # writing trees to files
+    println("Use 'write_yaml(tree, filename)' to write the edited tree to a file.")
+    pj.write_yaml(tree, expand_file)
+    println("Wrote tree to 'expand.pals.yaml'\n\n\n")
+
+    println("========== Printing Final Modified Tree ==========")
+    println(pj.to_yaml_string(tree))
+end
+
+main()
