6065657c06dae3c7d281bf2ddefa3be55d5e5395
eiad-hamwi <ehamwi@bnl.gov>
2026-06-25 19:48:46 -0400

    `PALS/facility` -> Bmad translator
    Handles single lattice files with multiple branchs (producing one `use` statement)
    
    Converts all magnetic fields to thin `AB_Multipole`
    
    Handles the following element parameters:
    - ApertureP
    - BendP
    - BodyShiftP
    - MagneticMultipoleP
    - PatchP
    - RFP
    - SolenoidP
    - ReferenceChangeP
    
    Does not handle:
    - ACKickerP
    - BeamBeamP
    - ElectricMultipoleP
    - FloorP
    - FloorShiftP
    - ForkP
    - GirderP
    - MetaP
    - TrackingP
    
    Does not handle constants or overlays.

---
+++ examples/example_pals_toBmad.jl
@@ -0,0 +1,9 @@
+if abspath(PROGRAM_FILE) == @__FILE__
+    pals_dir = joinpath(@__DIR__, "..")
+    ex_file     = joinpath(pals_dir, "lattice_files", "bta.pals.yaml")
+
+    include(joinpath(pals_dir, "src", "toBmad.jl"))
+
+    # Produces a file "pals_julia/lattice_files/bta.pals_out.bmad"
+    main(ex_file)
+end
